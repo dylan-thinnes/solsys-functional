1cfe4ad765712008cda96e6165fbbf468a59d157
dylan-thinnes <dylan.thinnes@protonmail.com>
2018-09-18 13:49:26 +0100

    Fix primecount glue to return all characters

--- backend/Primes/glue.cc
+++ backend/Primes/glue.cc
@@ -1,10 +1,12 @@
 #include <primecount.hpp>
+#include <stdio.h>
 #include <string>
 
 extern "C" {
     const char * pi_extern(char * input) {
+        char out[400];
         std::string x = std::string(input);
         std::string res = primecount::pi(x);
-        return res.c_str();
+        return strcpy(out, res.c_str());
     }
 }
