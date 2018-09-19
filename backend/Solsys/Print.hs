7806df60bdf0ed7e09a98bfb975ff5f3ddfe5294
dylan-thinnes <dylan.thinnes@protonmail.com>
2018-09-19 17:23:06 +0100

    Add JSON printer for Solsys

---
+++ backend/Solsys/Print.hs
@@ -0,0 +1,38 @@
+-- Pretty printing of Solsys modules to different formats
+module Solsys.Print where
+
+import Solsys.Planets
+
+import Data.Maybe (fromMaybe)
+import Data.List (intersperse)
+
+data JSON = JO [(String, JSON)]
+          | JA [JSON]
+          | JS String
+          | JI Integer
+          | Null
+
+getAttr :: JSON -> String -> JSON
+getAttr (JO o) s = fromMaybe Null $ lookup s o
+getAttr _ _ = Null
+
+getIndex :: JSON -> Int -> JSON
+getIndex (JA xs) ii = xs !! ii
+getIndex _ _ = Null
+
+planetTypeToJSON :: PlanetType -> JSON
+planetTypeToJSON t = JS $ show t
+
+planetToJSON :: Planet -> JSON
+planetToJSON (Planet t cs) = 
+    JO [("type", planetTypeToJSON t)
+       ,("children", JA $ map planetToJSON cs)
+       ]
+
+instance Show JSON where
+    show (JO kvs) = "{" ++ (concat . intersperse "," . map showKv $ kvs) ++ "}"
+                    where
+                    showKv (k,v) = "\"" ++ k ++ "\":" ++ show v
+    show (JA vs)  = "[" ++ (concat . intersperse "," . map show $ vs) ++ "]"
+    show (JS s) = show s
+    show (JI i) = show i
