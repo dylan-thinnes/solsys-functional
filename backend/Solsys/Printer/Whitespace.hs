2772a624a774cb0bde362d2decc750152e091ce7
dylan-thinnes <dylan.thinnes@protonmail.com>
2018-09-19 18:01:25 +0100

    Change format, improve example

--- backend/Solsys/Printer/Whitespace.hs
+++ backend/Solsys/Printer/Whitespace.hs
@@ -1,11 +1,14 @@
 {- Pretty printing of Solsys modules to JSON format
-E.g. 15 is equal to
+E.g. 15/7 is equal to
 positive
     neutral
     positive
     positive
+    negative
 Which is shortened to
-p
- n
- p
- p
++
+ 0
+ +
+ +
+ -
+-}
