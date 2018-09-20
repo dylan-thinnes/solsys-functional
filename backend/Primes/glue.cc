70249f6cb640800c4cca8ab1de2b595bf3737fd0
dylan-thinnes <dylan.thinnes@protonmail.com>
2018-09-20 08:50:13 +0100

    Fix memory allocation in pi_extern

--- backend/Primes/glue.cc
+++ backend/Primes/glue.cc
@@ -3,8 +3,8 @@
 #include <string>
 
 extern "C" {
-    const char * pi_extern(char * input) {
-        char out[400];
+    char * pi_extern(char * input) {
+        char * out = (char *) malloc(400);
         std::string x = std::string(input);
         std::string res = primecount::pi(x);
         return strcpy(out, res.c_str());
