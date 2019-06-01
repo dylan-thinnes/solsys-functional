5b48c9a2790b0a540b415bd1950c3ebfbdc126ab
Dylan Thinnes <dylan.thinnes@protonmail.com>
2019-06-01 11:24:16 +0100

    Add basic server for serving profiles

---
+++ backend/server.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Main where
+
+import Network.Wai
+import Network.HTTP.Types
+import Network.Wai.Handler.Warp
+
+import Control.Monad (join)
+import Text.Read (readMaybe)
+import Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString.Lazy.Char8 as BS
+
+import Solsys.Planets
+import qualified Solsys.Printer.JSON as J
+import qualified Solsys.Printer.Whitespace as W
+
+main :: IO ()
+main = run 3000 app
+
+app :: Application
+app req respond = do
+    putStrLn "Request received!"
+    let number :: Maybe Integer
+        number = join $ fmap (readMaybe . unpack) 
+               $ join $ lookup "number" $ queryString req
+    case number of 
+        Nothing 
+         -> do
+             putStrLn "Faulty request received."
+             respond $ responseLBS status400 defaultHeaders "{}\n"
+        Just n  
+         -> do
+             putStrLn $ "Profile for: " ++ show n
+             respond $ responseLBS status200 defaultHeaders
+                (BS.pack . (++"\n") . show . J.convert . rootPlanet $ n)
+
+defaultHeaders = [("Content-Type", "application/json")
+                 ,("Access-Control-Allow-Origin", "*")
+                 ,("Access-Control-Allow-Headers", "*")
+                 ]
